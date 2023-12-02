6c1e4767cefbbde96b61103f3878013480873c19
TheRyanMajd <rm97798@uga.edu>
2023-12-02 15:05:40 -0500

    Add compile and run scripts

---
+++ compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+javac -d bin -cp bin src/cs2720/p4/Sorting.java
+javac -d bin -cp bin src/cs2720/p4/Driver.java
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -cp bin src/cs2720/p4/Driver.java resources/random.txt
+# Will run random txt file 
